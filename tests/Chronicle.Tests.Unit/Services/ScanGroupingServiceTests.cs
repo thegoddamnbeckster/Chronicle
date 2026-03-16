d3496937a7da9379ec6f89494c96a5c954276b8f
Michael Beck <thegoddamnbeckster@users.noreply.github.com>
2026-03-16 12:37:41 -0600

    feat(services): add FolderSignalExtractor for hierarchy inference

--- tests/Chronicle.Tests.Unit/Services/ScanGroupingServiceTests.cs
+++ tests/Chronicle.Tests.Unit/Services/ScanGroupingServiceTests.cs
@@ -1,4 +1,5 @@
 using Chronicle.Core.Models.Scan;
+using Chronicle.Services.Scan;
 using FluentAssertions;
 
 namespace Chronicle.Tests.Unit.Services;
@@ -23,5 +24,39 @@ public class ScanGroupModelTests
 
         root.Children.Should().HaveCount(2);
         root.TotalFileCount.Should().Be(0);
+    }
+}
+
+public class FolderSignalExtractorTests
+{
+    private readonly FolderSignalExtractor _extractor = new();
+
+    [Theory]
+    [InlineData(@"C:\Music\Metallica\Black Album\01 Enter Sandman.mp3", @"C:\Music", 3, "Metallica", "Black Album", "01 Enter Sandman")]
+    [InlineData(@"C:\Music\Metallica\01 Enter Sandman.mp3", @"C:\Music", 2, "Metallica", null, "01 Enter Sandman")]
+    public void Extract_ReturnsCorrectHierarchyLevels(
+        string filePath, string scanRoot, int expectedHierarchyLevels,
+        string expectedLevel0, string? expectedLevel1, string expectedLeaf)
+    {
+        var result = _extractor.Extract(filePath, scanRoot);
+
+        result.HierarchyDepth.Should().Be(expectedHierarchyLevels);
+        result.FolderNames[0].Should().Be(expectedLevel0);
+        if (expectedLevel1 != null)
+            result.FolderNames[1].Should().Be(expectedLevel1);
+        result.FileName.Should().Be(expectedLeaf);
+    }
+
+    [Theory]
+    [InlineData(@"C:\TV\Breaking Bad\Season 1\S01E01 Pilot.mkv", "Breaking Bad", 1, 1)]
+    [InlineData(@"C:\TV\Breaking Bad\Season 5\S05E14 Ozymandias.mkv", "Breaking Bad", 5, 14)]
+    public void Extract_DetectsSeasonAndEpisodeFromFilename(
+        string filePath, string showName, int season, int episode)
+    {
+        var result = _extractor.Extract(filePath, @"C:\TV");
+
+        result.FolderNames[0].Should().Be(showName);
+        result.DetectedSeason.Should().Be(season);
+        result.DetectedEpisode.Should().Be(episode);
     }
 }
