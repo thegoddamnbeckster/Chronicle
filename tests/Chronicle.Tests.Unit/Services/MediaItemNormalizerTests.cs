a95a021ba8db31d3e5bef76c812a28c68e136585
Michael Beck <thegoddamnbeckster@users.noreply.github.com>
2026-06-03 16:29:57 -0600

    feat(services): add MediaItemNormalizer and startup normalized_name backfill

---
+++ tests/Chronicle.Tests.Unit/Services/MediaItemNormalizerTests.cs
@@ -0,0 +1,21 @@
+using Chronicle.Services;
+using FluentAssertions;
+
+namespace Chronicle.Tests.Unit.Services;
+
+public class MediaItemNormalizerTests
+{
+    [Theory]
+    [InlineData("James S. A. Corey",  "james s a corey")]
+    [InlineData("James S.A. Corey",   "james sa corey")]
+    [InlineData("James S.A.Corey",    "james sacorey")]
+    [InlineData("Brandon Sanderson",  "brandon sanderson")]
+    [InlineData("The Way of Kings",   "the way of kings")]
+    [InlineData("Abbey Road",         "abbey road")]
+    [InlineData("",                   "")]
+    [InlineData(null,                 "")]
+    public void NormalizeName_VariousInputs_CorrectResult(string? input, string expected)
+    {
+        MediaItemNormalizer.NormalizeName(input).Should().Be(expected);
+    }
+}
